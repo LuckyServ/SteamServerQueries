73770ec3a85cb88b8964d0afbc6d6761d5c3c6c8
Luckylock <luckylockgames@gmail.com>
2019-07-27 11:37:47 -0400

    A2S_INFO: Improved code quality

--- steamGameServer_A2S_INFO.py
+++ steamGameServer_A2S_INFO.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#
 # Python script to make A2S_INFO queries to steam game servers.
 #
 # Arguments: -v for verbose output
@@ -12,20 +13,27 @@ import socket
 import sys
 import binascii
 
+# List of servers to query
 IP_PORT = ([
     "192.223.24.83:27015", 
     "192.223.30.176:27015", 
     "140.82.26.135:27015",
 ])
 
+# Formatting
 LINE_SEP = "----------------------------------------"
+FIELD_SEP = " : "
 LJUST_VALUE = 11 
 
+# A2S_INFO values from Valve documentation
 A2S_INFO = binascii.unhexlify("FFFFFFFF54536F7572636520456E67696E6520517565727900")
 A2S_INFO_START_INDEX = 6
 A2S_INFO_STRING_ARRAY = ( 
     ["Name", "Map", "Folder", "Game"]
 )
+
+STEAM_PACKET_SIZE = 1400
+TIMEOUT = 0.5
 
 # Prints a string from the UDP packet recieved by the steam server.
 #
@@ -35,12 +43,17 @@ A2S_INFO_STRING_ARRAY = (
 # @return start index of the next info
 def printInfo(infoName, data, i):
     strFromBytes = ""
+
+    # Assemble string until null byte is found
     while data[i] != 0:
         strFromBytes = strFromBytes + chr(data[i])
         i = i + 1
+
+    # Print the string value
     if not(not(isVerbose) and (infoName == "Folder" or infoName == "Game")):
-        print(infoName.ljust(LJUST_VALUE) + " : " + strFromBytes)
+        print(infoName.ljust(LJUST_VALUE) + FIELD_SEP + strFromBytes)
     i = i + 1
+
     return i
 
 isVerbose = len(sys.argv) >= 2 and sys.argv[1] == "-v"
@@ -49,36 +62,72 @@ print(LINE_SEP)
 
 # Iterate over all IP addresses to print their A2S_INFO values
 for ipPort in IP_PORT:
-    print("Server".ljust(LJUST_VALUE) + " : " + ipPort)
+    print("Server".ljust(LJUST_VALUE) + FIELD_SEP + ipPort)
 
     # Send A2S_INFO request and get response from steam game server
     try:
         ipPortSplit = ipPort.split(":")
+
+        # Prep socket for UDP
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(0.5)
+
+        # Don't wait forever for a response
+        sock.settimeout(TIMEOUT)
+
+        # Send steam game server request
         sock.sendto(A2S_INFO, (ipPortSplit[0], int(ipPortSplit[1])))
-        data, addr = sock.recvfrom(1024)
+
+        # Get answer from server
+        data, addr = sock.recvfrom(STEAM_PACKET_SIZE)
+
+        # Done
         sock.close
     except:
         print(" " * LJUST_VALUE + "   CONNECTION FAILED")
         print(LINE_SEP)
         continue
 
-    # Convert data to bytearray 
     data = bytearray(data)
 
-    # Print the strings
+    # Print strings
     i = A2S_INFO_START_INDEX
     for infoName in A2S_INFO_STRING_ARRAY:
         i = printInfo(infoName, data, i)
 
-    # Print the numeric values
-    if isVerbose: print("ID".ljust(LJUST_VALUE) + " : " + str((data[i]) + (data[i+1] << 8)))
-    print("Players".ljust(LJUST_VALUE) + " : " + str(data[i+2]))
-    if isVerbose: print("Max Players".ljust(LJUST_VALUE) + " : " + str(data[i+3]))
-    if isVerbose: print("Bots".ljust(LJUST_VALUE) + " : " + str(data[i+4]))
-    if isVerbose: print("Server type".ljust(LJUST_VALUE) + " : " + ("dedicated server" if chr(data[i+5]) == 'd' else "non-dedicated server" if chr(data[i+5]) == 'l' else "SourceTV relay (proxy)"))
-    if isVerbose: print("Environment".ljust(LJUST_VALUE) + " : " + ("Linux" if chr(data[i+6]) == 'l' else "Windows" if chr(data[i+6]) == 'w' else "Mac"))
-    if isVerbose: print("Visibility".ljust(LJUST_VALUE) + " : " + ("private" if data[i+7] else "public"))
-    if isVerbose: print("VAC".ljust(LJUST_VALUE) + " : " + ("secured" if data[i+8] else "unsecured"))
+    # Print numeric values
+    print(
+        "Players".ljust(LJUST_VALUE) + FIELD_SEP 
+        + str(data[i+2])
+    )
+
+    if isVerbose: 
+        print(
+            "ID".ljust(LJUST_VALUE) + FIELD_SEP 
+            + str((data[i]) + (data[i+1] << 8))
+        )
+        print(
+            "Max Players".ljust(LJUST_VALUE) + FIELD_SEP 
+            + str(data[i+3])
+        )
+        print(
+            "Bots".ljust(LJUST_VALUE) + FIELD_SEP 
+            + str(data[i+4])
+        )
+        print(
+            "Server type".ljust(LJUST_VALUE) + FIELD_SEP 
+            + ("dedicated server" if chr(data[i+5]) == 'd' else "non-dedicated server" if chr(data[i+5]) == 'l' else "SourceTV relay (proxy)")
+        )
+        print(
+            "Environment".ljust(LJUST_VALUE) + FIELD_SEP 
+            + ("Linux" if chr(data[i+6]) == 'l' else "Windows" if chr(data[i+6]) == 'w' else "Mac")
+        )
+        print(
+            "Visibility".ljust(LJUST_VALUE) + FIELD_SEP 
+            + ("private" if data[i+7] else "public")
+        )
+        print(
+            "VAC".ljust(LJUST_VALUE) + FIELD_SEP 
+            + ("secured" if data[i+8] else "unsecured")
+        )
+
     print(LINE_SEP)
